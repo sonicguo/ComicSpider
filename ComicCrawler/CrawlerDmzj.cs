ea2b96a6e26faeec8a1c2069080a7fe5e8c371ea
Sonic Guo <sonicguo@hotmail.com>
2016-08-15 16:34:50 +0800

    commit solution and config files

---
+++ ComicCrawler/CrawlerDmzj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abot.Crawler;
+using Abot.Poco;
+
+
+
+namespace ComicCrawler
+{
+    public class CrawlerDmzj : BasicCrawler
+    {
+        public CrawlerDmzj()
+        {
+
+        }
+    }
+}
